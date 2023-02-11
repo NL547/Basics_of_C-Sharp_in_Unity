3dc10fa140e38e23e68c60c6dc5d34ee15b2d38a
NL547 <118132593+NL547@users.noreply.github.com>
2023-02-12 00:16:59 +0300

    Lesson_1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 2;
+
+    private Rigidbody componentRigidbody;
+
+    void Start()
+    {
+        componentRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        componentRigidbody.velocity = Vector2.zero;
+        if (Input.GetKey(KeyCode.A)) componentRigidbody.velocity += Vector3.left * Speed;
+        if (Input.GetKey(KeyCode.D)) componentRigidbody.velocity += Vector3.right * Speed;
+        if (Input.GetKey(KeyCode.W)) componentRigidbody.velocity += Vector3.forward * Speed;
+        if (Input.GetKey(KeyCode.S)) componentRigidbody.velocity += Vector3.back * Speed;
+    }
+}
